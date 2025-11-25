e8e15e0658af96235596a7f1d56de93fb4c79cec
Vitalii Boichenko <74453052+Hita4i@users.noreply.github.com>
2025-11-25 10:11:57 +0200

    Create MAVLink.h

---
+++ WinFormsApp1/MAVLink.h
@@ -0,0 +1,6 @@
+// MAVLink-Arduino
+// Author: Oleg Kalachev <okalachev@gmail.com>
+// Repository: https://github.com/okalachev/mavlink-arduino
+// Licence: MIT
+
+#include "C:/Users/parfv/OneDrive/Documents/Arduino/libraries/MAVLink/mavlink.h"
